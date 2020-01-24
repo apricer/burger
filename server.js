ea78518c5a078ffd2f4b47ac97ca6e2f8d5ece63
Anthony Pricer <anthony.pricer@outlook.com>
2020-01-23 19:33:41 -0600

    burger

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8000;
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("Listening on port:%s", PORT);
+});

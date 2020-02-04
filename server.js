2eed2fe54a593a2fe703bf4a8a5e94f8b20e6f2a
Anthony Pricer <anthony.pricer@outlook.com>
2020-02-03 22:49:41 -0600

    burger

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgersController.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
